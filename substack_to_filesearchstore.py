5580bcac6692af2e98319d6e7f343c6403a04bef
Anthony Garcia <acgarcia21@gmail.com>
2026-02-12 05:36:15 +0000

    Introduce substack_to_filesearchstore and new chat_server substack endpoint

--- substack_to_filesearchstore.py
+++ substack_to_filesearchstore.py
@@ -23,7 +23,7 @@ from dotenv import load_dotenv
 from google import genai
 
 # === CONFIGURATION ===
-ENV_FILE_PATH = r'C:\Users\acgar\OneDrive\Documents\GoogleAI\.env'
+ENV_FILE_PATH = r'.//.env'
 STORE_DISPLAY_NAME = 'SubstackBlog'
 
 
